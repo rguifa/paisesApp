5a7da8b3d546931e7d3048967bb95709f0c260be
Rubens Guifarro <rguifa@gmail.com>
2021-10-20 16:15:48 -0600

    Correccion API countries

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -8,13 +8,13 @@ import { Country } from '../interfaces/pais.interface';
 })
 export class PaisService {
 
-  private apiUrl: string = 'https://restcountries.eu/rest/v2'
+  private apiUrl: string = 'https://restcountries.com/v2'
 
   constructor(private http: HttpClient ) { }
 
   get httpParams (){
     return new HttpParams()
-    .set('fields', 'name;capital;alpha2Code;flag;population');
+    .set('fields', 'name,capital,alpha2Code,flag,population');
 
   }
 
@@ -22,6 +22,7 @@ export class PaisService {
 
     const url = `${ this.apiUrl }/name/${ termino }`;
     return this.http.get<Country[]>( url, { params: this.httpParams} );
+    
   }
 
   buscarCapital( termino: string ): Observable<Country[]>{
